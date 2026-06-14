3c00b6a76f1291d1d1c27d50ba8b71e9b6c68cbc
Lucas <lucassivolella@gmail.com>
2026-06-14 12:23:54 -0300

    feat: primeiro commit com Spring Boot e conexão ao Postgres

---
+++ backend/src/main/java/com/lucassivolella/webchamados_backend/WebchamadosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucassivolella.webchamados_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchamadosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebchamadosBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucassivolella/webchamados_backend/WebchamadosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassivolella.webchamados_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchamadosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
